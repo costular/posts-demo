8b2a4653dc1a1a320e687fedd25d127ffb14205c
Diego <costular@gmail.com>
2020-03-24 18:38:02 +0000

    Move MainActivity
    Move MainActivity to the presentation package

--- app/src/main/java/com/costular/postsdemo/presentation/MainActivity.kt
+++ app/src/main/java/com/costular/postsdemo/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.costular.postsdemo
+package com.costular.postsdemo.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.costular.postsdemo.R
 
 class MainActivity : AppCompatActivity() {
 
