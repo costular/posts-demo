1bdca5c8dac90caf97f7b4817a180353161d1f7a
Diego <costular@gmail.com>
2020-03-25 14:35:51 +0000

    Add tests & fix post remote

---
+++ app/src/test/java/com/costular/postsdemo/data/repository/datasource/comment/CommentRemoteDataSourceTest.kt
@@ -0,0 +1,4 @@
+package com.costular.postsdemo.data.repository.datasource.comment
+
+class CommentRemoteDataSourceTest {
+}
---
+++ app/src/test/java/com/costular/postsdemo/data/repository/datasource/user/UserRemoteDataSourceTest.kt
@@ -0,0 +1,4 @@
+package com.costular.postsdemo.data.repository.datasource.user
+
+class UserRemoteDataSourceTest {
+}
